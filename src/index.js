8c9eb39c7e74976a16314657be1cb5430bcc91d6
Rakel Björt <rakelbjort87@gmail.com>
2018-05-21 12:32:01 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as contentful from 'contentful';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+var client = contentful.createClient({
+  space: 'ifxfj736xke9',
+  accessToken:
+    '66d3883c10bf3366b9833ce4c5577ac2c6aeef69e48984f404b2643d437e44ed'
+});
+client
+  .getEntries({
+    content_type: 'singlePost'
+  })
+  .then(entries => {
+    console.log('her');
+    entries.items.forEach(entry => {
+      if (entry.fields) {
+        console.log(entry.fields);
+      }
+    });
+  });
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
