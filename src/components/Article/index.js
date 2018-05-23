ff1d326fde59a4cf6a9744ebdc0b830949ced12e
Rakel Björt <rakelbjort87@gmail.com>
2018-05-23 09:09:40 +0000

    chore: eject CRA and set up sass

---
+++ src/components/Article/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+export default class Article extends Component {
+  render() {
+    return (
+      <div>
+        <h1>test</h1>
+      </div>
+    );
+  }
+}
